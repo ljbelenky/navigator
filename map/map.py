12a0e40e1c7e492a2b459c0719f74c8ce622ac10
ljbelenky <ljbelenky@gmail.com>
2018-07-24 13:20:22 -0600

    culling isolated and dead-end nodes

--- map/map.py
+++ map/map.py
@@ -4,47 +4,61 @@ import matplotlib.pyplot as plt
 
 class Node():
     def __init__(self):
-        self.x = np.random.randint(1000)
-        self.y = np.random.randint(1000)
-        self.connections = {}
+        self.x = np.random.random()
+        self.y = np.random.random()
+        self.status = 'unvisited'
+        # self.connections = {}
     def __repr__(self):
-        return str(self.x) + " " + str(self.y)
+        return " ".join(['Node:', str(self.x), str(self.y)])
+
     def add_connection(self, other, factor):
         vector_x = self.x - other.x
         vector_y = self.y - other.y
         distance = (vector_x**2+vector_y**2)**.5
         self.connections[other] = distance * factor
-    def status(self, status):
-        '''start, stop, unvisited, visited, on-route'''
-        self.status = status
+
+    def distance(self,other):
+        a = np.array([self.x, self.y])
+        b = np.array([other.x, other.y])
+        return np.linalg.norm(a-b)
+
 
 class Map():
-    def __init__(self, nodes, connections):
+    def __init__(self, n_nodes, n_connections):
         # Make nodes
-        self.nodes = [Node() for _ in range(nodes)]
-        self.make_connections(connections)
+        self.nodes = [Node() for _ in range(n_nodes)]
+        self.make_connections(n_connections)
 
-    def make_connections(self,connections):
-        for _ in range(connections):
-            first = np.random.choice(self.nodes)
-            second = np.random.choice(self.nodes)
-            if first != second:
-                distance_factor = np.random.randint(100)
-                first.add_connection(second, distance_factor)
-                second.add_connection(first, distance_factor)
-        # delete any nodes with 0 connections
-        self.nodes = [node for node in self.nodes if len(node.connections)>0]
+    def make_connections(self, n_connections):
+        '''Connections are a dictionary with
+        key = tuple of two nodes
+        value = dictionary of duration and status
+        status = 'untraversed', 'traversed', 'on-route'
+        '''
+        self.connections = {}
+        for _ in range(n_connections):
+            key = tuple(np.random.choice(self.nodes, size = 2, replace = False))
+            duration = np.random.random()*key[0].distance(key[1])
+            status = 'untraversed'
+            self.connections[key] = {'duration':duration, 'status': status}
 
+    def remove_unconnected_nodes(self):
+        # remove any unconnected nodes
+        connected_nodes = set()
+        for n1, n2 in self.connections:
+            connected_nodes |= set([n1, n2])
+
+        self.nodes = [node for node in self.nodes if node in connected_nodes]
+
+    def pick_start_and_stop(self):
         # pick start and stop node
         self.start = self.nodes[0]
+        self.start.status = 'start'
         self.stop = self.nodes[-1]
+        self.stop.status = 'stop'
 
     def __repr__(self):
-        total_connections = 0
-        for node in self.nodes:
-            total_connections += len(node.connections)
-        total_connections /= 2
-        return "map with {} nodes and {} connections".format(len(self.nodes), total_connections)
+        return "map with {} nodes and {} connections".format(len(self.nodes), len(self.connections))
 
     def plot(self):
         for node in self.nodes:
@@ -53,10 +67,8 @@ class Map():
             else:
                 c = 'gray'
             plt.scatter(node.x, node.y, marker = 'o', c=c)
-            for connection in node.connections:
-                xx = [node.x, connection.x]
-                yy = [node.y, connection.y]
-                plt.plot(xx,yy, c= 'gray', alpha = .2)
+        for n1, n2 in self.connections:
+            plt.plot([n1.x, n2.x],[n1.y, n2.y], c= 'gray', alpha = .2)
         plt.show()
 
     def histogram(self):
@@ -64,13 +76,49 @@ class Map():
         plt.hist(data)
         plt.show()
 
-    def relax(self):
-        '''for each node, find vector to all connected points, apply connected force
-        find vector to all unconnected points, apply unconnected force'''
+    def remove_dead_ends(self):
+        relaxing_rate = .01
 
-        for node in self.nodes:
-            # connected_nodes = pd.DataFrame()
 
-            # unconnected_nodes = pd.DataFrame()
-            node.x += delta_connected_x + delta_unconnected_x
-            node.y += delta_connected_y + delta_unconnected_y
+    # remove any nodes with 1 connection that are not start or stop
+        from collections import Counter
+        #make repetitive list of all nodes in connections
+        connected_nodes = []
+        for n1, n2 in self.connections:
+            connected_nodes.extend([n1,n2])
+        counter = Counter(connected_nodes)
+        #make list of all dead ends that have 1 connection and are not start or stop
+        dead_ends = []
+        for node, count in counter.items():
+            if node.status not in ['start','stop'] and count == 1:
+                dead_ends.append(node)
+        print(dead_ends)
+        self.nodes = list(set(self.nodes) - set(dead_ends))
+        # delete all connections that don't have both ends in nodes
+        culled_connections = {}
+        for (n1, n2), values in self.connections.items():
+            if n1 in self.nodes and n2 in self.nodes:
+                culled_connections[(n1,n2)]=values
+        self.connections = culled_connections
+
+
+
+
+    #     '''for each node, find vector to all connected points, apply connected force
+    #     find vector to all unconnected points, apply unconnected force'''
+    '''Connected Force:
+    x<2d: force = 1-x/d
+    x>2d: force = -1
+    Unconnected Force:
+    x<1: force = 1-x
+    x>1 force = 0
+    '''
+    #
+    #     for node in self.nodes:
+    #         for connection in node.connections:
+    #
+    #         # connected_nodes = pd.DataFrame()
+    #
+    #         # unconnected_nodes = pd.DataFrame()
+    #         node.x += delta_connected_x + delta_unconnected_x
+    #         node.y += delta_connected_y + delta_unconnected_y
--- navigator.py
+++ navigator.py
@@ -1,9 +1,20 @@
 from map import map
 
 
-m = map.Map(50, 20)
-for node in m.nodes:
-    print(node.connections)
+m = map.Map(50, 100)
+m.pick_start_and_stop()
+# for node in m.nodes:
+#     print(node.connections)
 print(m)
 m.plot()
-m.histogram()
+
+nodes, connections = len(m.nodes), len(m.connections)
+while True:
+    m.remove_unconnected_nodes()
+    m.remove_dead_ends()
+    print(m)
+    m.plot()
+    if nodes == len(m.nodes) and connections == len(m.connections):
+        break
+    else:
+        nodes, connections = len(m.nodes), len(m.connections)
